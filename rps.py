a8d513b61b4395b2c2df62ba80294a0364f8ecd8
Davidsenec <sgd88.thomponsenec@yahoo.com>
2025-03-22 17:29:51 +0700

    Made a new repo with new mini Python Project

---
+++ rps.py
@@ -0,0 +1,32 @@
+import random
+
+# A function that asks the player for their choice
+def getchoice():
+    p_choice = input("Choose your weapon!:")
+    options = ["rock", "paper", "scissors"]
+    c_choice = random.choice(options)
+    choices =  {"Player:":p_choice, "Computer:":c_choice}
+
+    return choices  
+
+# A function that checks if the player wins or loses
+def checkwin(Player, Computer):
+    print(f"You chose {Player}, the computer chose {Computer}")
+    if Player == Computer:
+        return "Draw"
+    elif Player == "rock":
+        if Computer == "scissors":
+            return "Rock destroys scissors. You Win!"
+        elif Computer == "paper":
+            return "Paper beats Rock. You lose STUPID!"
+
+
+# Prints the welcome message
+print("Welcome to Rock Paper Scissors!")
+
+# Asks the player for their choice
+choices = getchoice()
+
+# Prints what the player chose along with the computer
+print(choices)
+checkwin("rock", "scissors")
