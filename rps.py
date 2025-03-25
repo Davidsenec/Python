27c5d5861fd6d5cd7dc8f548612ede8d6d7ab033
Davidsenec <sgd88.thomponsenec@yahoo.com>
2025-03-25 20:17:11 +0700

    Fixed format for this branch

--- rps.py
+++ rps.py
@@ -1,5 +1,6 @@
 import random
 
+
 # A function that asks the player for their choice
 def getchoice():
     p_choice = input("Choose your weapon!:").strip().lower()
@@ -9,23 +10,24 @@ def getchoice():
     while p_choice not in options:
         print("Invalid input. Try again.")
         p_choice = input("Choose your weapon!:").strip().lower()
-    
+
     c_choice = random.choice(options)
-    choices =  {"Player":p_choice, "Computer":c_choice}
-    
-    return choices
+    player_choices = {"Player": p_choice, "Computer": c_choice}
+
+    return player_choices
+
 
 # A function that checks if the player wins or loses
-def checkwin(Player, Computer):
-    print(f"You chose {Player}, the computer chose {Computer}")
-    if Player == Computer:
+def check_win(player, computer):
+    print(f"You chose {player}, the computer chose {computer}")
+    if player == computer:
         return "Draw"
-    elif Player == "rock" and Computer == "scissors" or \
-    Player == "scissors" and Computer == "paper" or \
-    Player == "paper" and Computer == "rock":
-        return f"{Player} beats {Computer} You Win!"
+    elif player == "rock" and computer == "scissors" or \
+            player == "scissors" and computer == "paper" or \
+            player == "paper" and computer == "rock":
+        return f"{player} beats {computer} You Win!"
     else:
-        return f"{Computer} beats {Player} You Lose STUPID!"
+        return f"{computer} beats {player} You Lose STUPID!"
 
 
 # Prints the welcome message
@@ -34,7 +36,7 @@ print("Welcome to Rock Paper Scissors!")
 # Asks the player for their choice
 choices = getchoice()
 # calls the win condition function
-result = checkwin(choices["Player"], choices["Computer"])
+result = check_win(choices["Player"], choices["Computer"])
 
 # Prints what the player chose along with the computer and who wins
 print(choices)
